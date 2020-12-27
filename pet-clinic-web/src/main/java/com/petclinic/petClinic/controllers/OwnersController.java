be5a3eb30942c1ef098fd92ba2911c4ad816652d
Prabuddha <prabuddha.proart@gmail.com>
2020-12-28 02:40:44 +0530

    adding owner listing and send model from controller

--- pet-clinic-web/src/main/java/com/petclinic/petClinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/com/petclinic/petClinic/controllers/OwnersController.java
@@ -1,14 +1,24 @@
 package com.petclinic.petClinic.controllers;
 
+import com.petclinic.petClinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnersController {
 
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String ownersList(){
+    public String ownersList(Model model){
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
