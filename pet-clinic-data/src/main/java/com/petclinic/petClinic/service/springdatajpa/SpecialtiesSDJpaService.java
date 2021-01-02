7c7a4fa5489e8f9574c82e476582a9bc6a0e050b
Prabuddha <prabuddha.proart@gmail.com>
2021-01-02 12:15:08 +0530

    data JAP service for Pet, PetType, Vet and Specialties

---
+++ pet-clinic-data/src/main/java/com/petclinic/petClinic/service/springdatajpa/SpecialtiesSDJpaService.java
@@ -0,0 +1,49 @@
+package com.petclinic.petClinic.service.springdatajpa;
+
+
+import com.petclinic.petClinic.model.Speciality;
+import com.petclinic.petClinic.repositories.SpecialtyRepository;
+import com.petclinic.petClinic.service.SpecialtesService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialtiesSDJpaService implements SpecialtesService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtiesSDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
