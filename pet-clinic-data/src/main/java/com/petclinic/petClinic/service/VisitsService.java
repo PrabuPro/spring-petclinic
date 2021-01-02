084a99e9106beb5ca1472d2e171ac8e91add7790
Prabuddha <prabuddha.proart@gmail.com>
2021-01-02 14:13:50 +0530

    create map for visit service

---
+++ pet-clinic-data/src/main/java/com/petclinic/petClinic/service/VisitsService.java
@@ -0,0 +1,6 @@
+package com.petclinic.petClinic.service;
+
+import com.petclinic.petClinic.model.Visit;
+
+public interface VisitsService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/petclinic/petClinic/service/map/VisitServiceMap.java
@@ -0,0 +1,40 @@
+package com.petclinic.petClinic.service.map;
+
+import com.petclinic.petClinic.model.Visit;
+import com.petclinic.petClinic.service.VisitsService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitsService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+        || visit.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit) ;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
