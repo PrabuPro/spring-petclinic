b1aa3ed688af3287d7f9a24324e33dbcfd74e5f9
Prabuddha <prabuddha.proart@gmail.com>
2021-01-22 00:22:29 +0530

    find owners by name

--- pet-clinic-data/src/main/java/com/petclinic/petClinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/petclinic/petClinic/repositories/OwnerRepository.java
@@ -3,8 +3,11 @@ package com.petclinic.petClinic.repositories;
 import com.petclinic.petClinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
+    List<Owner> findAllByLastNameContainingIgnoreCase(String anyString);
 }
--- pet-clinic-data/src/main/java/com/petclinic/petClinic/service/OwnerService.java
+++ pet-clinic-data/src/main/java/com/petclinic/petClinic/service/OwnerService.java
@@ -2,8 +2,11 @@ package com.petclinic.petClinic.service;
 
 import com.petclinic.petClinic.model.Owner;
 
+import java.util.List;
 import java.util.Set;
 
 public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameContainingIgnoreCase(String anyString);
 }
